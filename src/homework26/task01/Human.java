82c594a2ef83658979ef9895d6520d01d0ef39b5
Vitalii Kovtun <kovvv1992@gmail.com>
2023-11-19 21:31:08 +0100

    add task01

---
+++ src/homework26/task01/Human.java
@@ -0,0 +1,10 @@
+package homework26.task01;
+
+public class Human {
+  String name;
+  int age;
+
+  public void introduce() {
+    System.out.println("Hi, my name is " + name + ". I'm " + age + " years old.");
+  }
+}
